0dc232f1c35b24493def7d39a76742a3b5c0fabd
KirillMelanich <kirillmelanich@gmail.com>
2023-07-17 11:05:26 +0300

    Remove redundant from catalog/views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
@@ -177,14 +176,12 @@ class SongDetailView(LoginRequiredMixin, generic.DetailView):
 class SongCreateView(LoginRequiredMixin, generic.CreateView):
     model = Song
     form_class = SongForm
-    # fields = "__all__"
     success_url = reverse_lazy("catalog:song-list")
 
 
 class SongUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Song
     form_class = SongForm
-    # fields = "__all__"
     success_url = reverse_lazy("catalog:song-list")
 
 

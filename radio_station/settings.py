d1a1805c614f84d5a4390192c536b5c06d2426db
KirillMelanich <kirillmelanich@gmail.com>
2023-07-17 14:10:35 +0300

    Changed Secret_key settings and .env

--- radio_station/settings.py
+++ radio_station/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1%l1pwb#o&x(&z2n5k1ku5nsu3=ai)u5i=nm%!gs4cg&=0_zrr"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
